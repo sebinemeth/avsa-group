5a14e2a775cfeff2f2a95811f91ae8da189696db
Sebi Nemeth <sebezhetetlen98@gmail.com>
2022-05-05 11:28:37 +0200

    add sample code

---
+++ lab4_sample_code/utils.hpp
@@ -0,0 +1,21 @@
+/* Applied Video Sequence Analysis - Escuela Politecnica Superior - Universidad Autonoma de Madrid
+ *
+ *	This source code belongs to the template code for
+ *	the assignment LAB 4 "Histogram-based tracking"
+ *
+ *	Header of utilities for LAB4.
+ *	Some of these functions are adapted from OpenSource
+ *
+ * Author: Juan C. SanMiguel (juancarlos.sanmiguel@uam.es)
+ * Date: April 2020
+ */
+#ifndef UTILS_HPP_
+#define UTILS_HPP_
+
+#include <string> 		// for string class
+#include <opencv2/opencv.hpp>
+
+std::vector<cv::Rect> readGroundTruthFile(std::string groundtruth_path);
+std::vector<float> estimateTrackingPerformance(std::vector<cv::Rect> Bbox_GT, std::vector<cv::Rect> Bbox_est);
+
+#endif /* UTILS_HPP_ */
